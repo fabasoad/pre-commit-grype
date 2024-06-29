f3a33894debd3c00b1fbb78a8b4d41ae17c95bc5
fabasoad <fabasoad@gmail.com>
2024-06-29 17:32:24 +0900

    Fix 1

--- src/lib/global-vars/set.sh
+++ src/lib/global-vars/set.sh
@@ -3,9 +3,9 @@
 MAIN_SCRIPT_PATH=$(realpath "$0")
 SRC_DIR_PATH=$(dirname "${MAIN_SCRIPT_PATH}")
 LIB_DIR_PATH="${SRC_DIR_PATH}/lib"
-ENV_VARS_DIR_PATH="${LIB_DIR_PATH}/env-vars"
+GLOBAL_VARS_DIR_PATH="${LIB_DIR_PATH}/global-vars"
 
-. "${ENV_VARS_DIR_PATH}/default.sh"
+. "${GLOBAL_VARS_DIR_PATH}/default.sh"
 
 set_global_log_level() {
   export PRE_COMMIT_GRYPE_LOG_LEVEL="$1"
