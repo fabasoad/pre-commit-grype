ba9b0fe2b901a4b6557b310911b49d6dba297733
fabasoad <fabasoad@gmail.com>
2024-07-14 23:54:57 +0900

    Remove bpkg prerequisites

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,12 +6,11 @@ ROOT_DIR="$(dirname "${SCRIPTS_DIR}")"
 bpkg_packages_dir="${ROOT_DIR}/src/.bpkg"
 
 main() {
-  rm -rf "${bpkg_packages_dir}"
   mkdir -p "${bpkg_packages_dir}"
   lock_file="${ROOT_DIR}/.bpkg.lock"
   while IFS= read -r line; do
-    PREFIX="${bpkg_packages_dir}" bpkg install "$line" >/dev/null
+    PREFIX="${bpkg_packages_dir}" bpkg install "$line"
   done < "${lock_file}"
 }
 
-main "$@"
+main $@
