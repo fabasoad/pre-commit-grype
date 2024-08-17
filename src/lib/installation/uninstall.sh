57943b71dd6d5f090ec77f0381a6fbc98ad3bf94
fabasoad <fabasoad@gmail.com>
2024-08-18 08:56:23 +0900

    Add another condition

--- src/lib/installation/uninstall.sh
+++ src/lib/installation/uninstall.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env sh
 
+_remove_dir() {
+  rm -rf "${1}"
+  fabasoad_log "debug" "${1} directory has been removed"
+}
+
 uninstall() {
   if [ "${PRE_COMMIT_GRYPE_CLEAN_CACHE}" = "true" ]; then
-    rm -rf "${CONFIG_CACHE_APP_DIR}"
-    fabasoad_log "debug" "${CONFIG_CACHE_APP_DIR} directory has been removed"
+    if [ -d "${CONFIG_CACHE_APP_DIR}" ]; then
+      _remove_dir "${CONFIG_CACHE_APP_DIR}"
+    fi
     if [ -d "${CONFIG_CACHE_ROOT_DIR}" ] && [ -z "$(ls -A "${CONFIG_CACHE_ROOT_DIR}" 2>/dev/null)" ]; then
-      rm -rf "${CONFIG_CACHE_ROOT_DIR}"
-      fabasoad_log "debug" "${CONFIG_CACHE_ROOT_DIR} directory has been removed since it is empty"
+      _remove_dir "${CONFIG_CACHE_ROOT_DIR}"
     fi
   else
     fabasoad_log "debug" "${CONFIG_CACHE_APP_DIR} directory was not removed"
